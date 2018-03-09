28a3979b12cbda51108bf2a3abd17c233825f664
Peter M. Gits <peter_gits@dell.com>
2018-03-08 16:45:35 -0800

    updated to offer a subscription for Wait For Merge

--- etc/merge-trigger.php
+++ etc/merge-trigger.php
@@ -1,5 +1,29 @@
 <?php 
 
+function fetch_last_package_that_passed_smoketest($DEBUG, $conn, $database, $branch, $packageName){
+	$myPackageRevision = "1";
+$PASSED_SMOKE_TEST=12;
+
+	$sqlString = "SELECT transactionid, packageRevision FROM $database.PackagesPassedSmokeTest WHERE branch = \"$branch\" AND packageName = \"$packageName\" AND operation = $PASSED_SMOKE_TEST ORDER BY transactionId DESC LIMIT 1;";
+	if($DEBUG == TRUE)
+		echo "\nfetch_last_package()= this is what we are sending to mySql:  [" . $sqlString . "]\n";
+	if($select_revision_result = mysqli_query($conn, $sqlString)){
+		$rows = array();
+  		$rowcount=mysqli_num_rows($select_revision_result);
+		if($DEBUG == TRUE)
+  			printf("Result set has %d rows.\n",$rowcount);
+		while($r = mysqli_fetch_assoc($select_revision_result)) {
+                	$rows[] = $r;
+			print_r($rows);
+			$myPackageRevision = $r["packageRevision"];
+			$packageRevisionJustNumber = substr($myPackageRevision, strrpos($myPackageRevision, ".") + 1);
+			print("packageRevision found = $packageRevisionJustNumber\n");
+			return $packageRevisionJustNumber;
+		}
+	}
+	return $myPackageRevision;
+}
+
 function fetch_last_package($DEBUG, $conn, $database, $branch, $packageName){
 	$myPackageRevision = "1";
 
@@ -22,6 +46,7 @@ function fetch_last_package($DEBUG, $conn, $database, $branch, $packageName){
 	}
 	return $myPackageRevision;
 }
+
 
 function mergeTrigger($branch, $packageName, $packageRevision, $jsonContainer, $mergeUser, $pullRequestId ){
 
@@ -76,7 +101,7 @@ if ($conn->connect_error) {
 
 		if($packageRevision == 0)
 		//need to locate the last packageRevision
-			$packageRevision = fetch_last_package($DEBUG, $conn, $database, $branch, $packageName);
+			$packageRevision = fetch_last_package_that_passed_smoketest($DEBUG, $conn, $database, $branch, $packageName);
 
 		//this is a trigger to start
 		$sqlString = "INSERT INTO " . $database . "." . $table . "(`packageName`, `packageRevision`, `branch`, `mergeStatus`, `pullRequestId`, `expires`) VALUES (\"$packageName\", \"$packageRevision\", \"$branch\", 1, $pullRequestId, DATE_ADD(CURRENT_DATE, INTERVAL 2 week) );";
@@ -101,21 +126,8 @@ if ($conn->connect_error) {
 				echo "transaction id now = [" . $transId . "]\n";
 				echo "call system call back to continuum with jsonContents and transactionId";
 			}
-
-			$ch = curl_init('http://veronecontinuum-eqx-01.force10networks.com:8080/api/promote_revision');
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_POST, 1);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, "package=$packageName&branch=$branch&revision=$packageRevision&phase=Trigger Merge Package Build");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-					'Authorization: Token 59553bf736ede316388f92ad'
-    				)
-			);                                                                                                                   
-			$curl_result = curl_exec($ch);
-			echo "response from curl = " . $curl_result . "\n";
-			curl_close($ch);
-			var_dump($curl_result);
+			//promote_revision($ch, $packageName, $branch, $packageRevision);
+			setapi($conn, $database, $packageName, $branch, $packageRevision);
 			mysqli_close($conn);
 			return true;
 		}
@@ -141,6 +153,83 @@ if ($conn->connect_error) {
 	mysqli_close($conn);
 	return true;
   }//end of else
+}
+
+function promote_revision($ch, $packageName, $branch, $packageRevision) {
+                        $ch = curl_init('http://veronecontinuum-eqx-01.force10networks.com:8080/api/promote_revision');
+                        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                        curl_setopt($ch, CURLOPT_POST, 1);
+                        curl_setopt($ch, CURLOPT_POSTFIELDS, "package=$packageName&branch=$branch&revision=$packageRevision&phase=Trigger Merge Package Build");
+                        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                                        'Authorization: Token 59553bf736ede316388f92ad'
+                                )
+                        );                                                                                                         
+                        $curl_result = curl_exec($ch);
+                        echo "response from curl = " . $curl_result . "\n";
+                        curl_close($ch);
+                        var_dump($curl_result);
+}
+
+function fetchSubscription($conn, $database, $packageName, $branch, &$pipeline_id, &$packageRevision){
+	$sqlString = "SELECT * from FROM $database.callBacks WHERE branch = \"$branch\" AND packageName = \"$packageName\" AND ORDER BY transactionId DESC;";
+	//return the rows with the continuumId and the revision	
+	$resSelect = mysqli_query($conn, $sqlString);
+	if($resSelect){
+		$rows = array();
+		if($resSelect)
+        	{
+                	while($r = mysqli_fetch_assoc($resSelect))
+                	{
+				$rows[] = $r;
+				print_r($r);
+				$pipeline_id = $r['pipeline_id'];
+				$packageRevision = $r['packageRevision'];
+				echo "packageRevision = $packageRevision\n";
+				echo "pipeline_id = $pipeline_id\n";
+			}
+		}
+	}
+}
+
+//calls fetchSubscription to pull down both packageRevision and the pipeline_id
+// and then uses that to continue the pipeline to the next phase
+
+function setapi($conn, $database, $packageName, $branch, $packageRevision, $imageEnterpriseFullPathAndName, $imageBaseFullPathAndName, $transactionId){
+	fetchSubscription($conn, $database, $packageName, $branch, $pipeline_id, $packageRevision);	
+	$packageRevisionJustNumber = substr($packageRevision, strrpos($packageRevision, ".") + 1);
+	$someJSON="{\"pi\": \"$pipeline_id\", " .
+			    "\"key\": \"returns\", \"value\": " .
+				"{\"list\": " .
+				   "\"version\": \"$packageRevisionJustNumber\", " .
+				   "\"transactionIdStretch\": \"$transactionId\", " .
+				   "\"package\": \"$packageName\", " .
+				   "\"branch\": \"$branch\", " .
+				   "\"tftpboot_enterprise_path\": \"$imageEnterpriseFullPathAndName\", " .
+				   "\"tftpboot_base_path\": \"$imageBaseFullPathAndName\", " .
+				   "\"transactionId\": \"$transactionId\" " .
+				"}" .
+		  "}";
+		// Convert JSON string to Array
+  	$someArray = json_decode($someJSON, true);
+  	print_r($someArray);        // Dump all data of the Array
+
+	$postJsonData = json_encode($someArray);
+	echo "postJsonData now = " . $postJsonData . "\n";
+
+	$ch = curl_init('http://veronecontinuum-eqx-01.force10networks.com:8080/api/set_pi_data');                                                                      
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $postJsonData);                                                                  
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+		'Authorization: Token 59553bf736ede316388f92ad',
+		'Content-Type: application/json',
+    		'Content-Length: ' . strlen($postJsonData)
+		)
+	);
+	$result = curl_exec($ch);
+	var_dump($result);
 }
 
 ?>
